51a25649e609ce2c3b2a76ba946e342495d656b8
Smartic Angola <smarticangola@gmail.com>
2025-09-07 17:05:35 +0000

    Try fixing this error: `Runtime Error: Error: onAddUpdate is not defined

--- src/components/main-page-handler.tsx
+++ src/components/main-page-handler.tsx
@@ -1174,7 +1174,7 @@ export default function MainPageHandler({ userMenu }: { userMenu: React.ReactNod
             }
             }}
             onPoiStatusChange={handlePoiStatusChange}
-            onAddUpdate={onAddUpdate}
+            onAddUpdate={handleAddUpdate}
             onEdit={handleStartEditing}
         />
         <IncidentReport 
@@ -1274,3 +1274,4 @@ export default function MainPageHandler({ userMenu }: { userMenu: React.ReactNod
       </SidebarProvider>
   );
 }
+
