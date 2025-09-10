b675e56aca7436af5fbca605bdcafdb2cf715e0d
Smartic Angola <smarticangola@gmail.com>
2025-09-10 15:02:59 +0000

    Agora vamos continuar a implementar o modulo Iluminação Pública O cadast

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -8,7 +8,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, DropdownMenuSub, DropdownMenuSubTrigger, DropdownMenuSubContent, DropdownMenuGroup } from "@/components/ui/dropdown-menu";
 import { UserProfile } from "@/lib/data";
-import { LayoutDashboard, LogOut, Megaphone, User as UserIcon, Users, FileText, Briefcase, ScanLine, Settings, Home, Building, ShieldCheck, Inbox, Droplets, GitBranch, Share2, AreaChart, Fuel, Wrench, Sun, Hospital, Stethoscope, CreditCard, Package, Bus, ListTodo, Lightbulb, Zap, HardHat } from "lucide-react";
+import { LayoutDashboard, LogOut, Megaphone, User as UserIcon, Users, FileText, Briefcase, ScanLine, Settings, Home, Building, ShieldCheck, Inbox, Droplets, GitBranch, Share2, AreaChart, Fuel, Wrench, Sun, Hospital, Stethoscope, CreditCard, Package, Bus, ListTodo, Lightbulb, Zap, HardHat, DollarSign } from "lucide-react";
 
 
 interface UserMenuProps {
